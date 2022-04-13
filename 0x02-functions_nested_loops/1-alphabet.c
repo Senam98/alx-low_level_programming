0c62dc0caa61d0d10a4ee83a841ec395364316ac
Gordon Enam Kudzo <myprivacy98@gmail.com>
2022-04-13 05:56:13 +0000

    My Nested Loop Tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints lowercase alphabet
- * Return: 0
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
 {
-	char alpha;
+	char c = 'a';
 
-	alpha = 'a';
-
-	while (alpha <= 'z')
+	while (c <= 'z')
 	{
-		_putchar(alpha);
-		alpha++;
+		_putchar(c);
+		c++;
+
 	}
 	_putchar('\n');
+
 }
