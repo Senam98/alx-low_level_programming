48752d2aa3e96d7586579a00cb29dad90f946692
Gordon Enam Kudzo <myprivacy98@gmail.com>
2022-04-21 02:23:45 +0000

    Task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,20 @@
-#include "main.h" 
-#include <stdio.h>
-
-/**
-  * _puts - prints a string, followed by a new line, 
-  * @str: pointer to the string to print 
-  * Return: void 
-  */
-
-void _puts(char *str) 
+#​include​ ​"​main.h​" 
+ ​/*​* 
+ ​ * _puts - prints a string, followed by a new line, 
+ ​ * @str: pointer to the string to print 
+ ​ * Return: void 
+ ​*/ 
+  
+  
+void​ ​_puts​(​char​ *str) 
 { 
-  int i = 0; 
-  while (str[i]) 
-{ 
-  _putchar(str[i]);
-    i++; 
-} 
-  _putchar('\n'); 
+ ​        ​int​ i = ​0​; 
+  
+ ​        ​while​ (str[i]) 
+ ​        { 
+ ​                ​_putchar​(str[i]); 
+ ​                i++; 
+ ​        } 
+  
+ ​        ​_putchar​(​'​\n​'​); 
 }
