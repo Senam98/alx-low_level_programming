329e27cc216a194ec9e654da71e7eb381d11a51b
Gordon Enam Kudzo <myprivacy98@gmail.com>
2022-04-21 04:57:44 +0000

    Task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,27 +1,24 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * puts2 - print character of a string
- *      in an array starting from the first
- *      character followed by the rest of the
- *      even index array
- *
- * @str: string input array pointer
- *
- * Return: nothing
+* puts2 - print character of a string
+* in an array starting from the first
+* character followed by the rest of the
+* even index array
+* @str: string input array pointer
+* Return: nothing
 */
 
 void puts2(char *str)
 {
-	int i;
+int i;
+for (i = 0; str[i] >= 0; i++)
+{
+if (str[i] == '\0')
+break;
 
-	for (i = 0; str[i] >= 0; i++)
-	{
-		if (str[i] == '\0')
-			break;
-
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
-	_putchar('\n');
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
 }
